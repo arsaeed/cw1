74d29cd6fef3c4b19fb2d5f1f3c2633bb328acf2
arsaeed <81184632+arsaeed@users.noreply.github.com>
2021-04-11 16:37:31 +0400

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -49,7 +49,7 @@ let lessons = [
         "subject": 'Medicine',
         "location": "Hendon",
         "price": 350,
-        "image": "https://images.app.goo.gl/CJPvExvsqCJT5ZLH7",
+        "image": "https://industrywired.com/wp-content/uploads/2020/01/Business-Tech.jpg",
         "availableInventory":5,
         "rating": 5
     },
